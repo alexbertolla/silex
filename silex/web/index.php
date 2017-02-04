c94ebc739c15a443fc8b183cb555ef022f8fb0b5
alex <alex@megaware>
2017-02-04 15:14:51 -0200

    Módulo Silex
    Controllers
    
    Projeto Fase 2

--- silex/web/index.php
+++ silex/web/index.php
@@ -20,33 +20,6 @@ $app = new \Silex\Application();
 $app['debug'] = TRUE;
 
 
-$posts = array(
-    array('id' => 1, 'post' => 'post1'),
-    array('id' => 2, 'post' => 'post2'),
-    array('id' => 3, 'post' => 'post3'),
-    array('id' => 4, 'post' => 'post4'),
-    array('id' => 5, 'post' => 'post5'),
-    array('id' => 6, 'post' => 'post6'),
-    array('id' => 7, 'post' => 'post7'),
-    array('id' => 8, 'post' => 'post8'),
-    array('id' => 9, 'post' => 'post9'),
-    array('id' => 10, 'post' => 'post10'),
-);
-
-
-
-$app->get('/post/{id}', function ($id) use($posts) {
-
-    if (is_numeric($id)) {
-        foreach ($posts as $post) {
-            if ($id == $post['id']) {
-                return $post['id'] . ' - ' . $post['post'];
-            }
-        }
-    } else {
-        echo "Valor do id {$id} inválido";
-    }
-});
-
+include_once './posts.php';
 
 $app->run();
---
+++ silex/web/posts.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Projeto fase 2 - Controllers
+
+  Nesta fase você deverá separar criar um arquivo separado para realocarmos as rotas de
+  posts e mover a lógica de listagem de um post para ele.
+
+  Você deverá criar uma outra rota (/posts) para listar todos posts e seus conteúdos,
+  além disto, nesta listagem você deverá criar uma âncora (link), para que possamos
+  ser redirecionados para listagem de um post apenas.
+ */
+$posts = array(
+    array('id' => 1, 'post' => 'post1'),
+    array('id' => 2, 'post' => 'post2'),
+    array('id' => 3, 'post' => 'post3'),
+    array('id' => 4, 'post' => 'post4'),
+    array('id' => 5, 'post' => 'post5'),
+    array('id' => 6, 'post' => 'post6'),
+    array('id' => 7, 'post' => 'post7'),
+    array('id' => 8, 'post' => 'post8'),
+    array('id' => 9, 'post' => 'post9'),
+    array('id' => 10, 'post' => 'post10'),
+);
+
+
+$app->get('/posts', function () use($posts) {
+    foreach ($posts as $post) {
+        $retorno .= "<a href='/post/{$post['id']}'>{$post['id']} - {$post['post']}</a><br>";
+    }
+    return $retorno;
+});
+
+
+$app->get('/post/{id}', function ($id) use($posts) {
+
+    if (is_numeric($id)) {
+        foreach ($posts as $post) {
+            if ($id == $post['id']) {
+                return $post['id'] . ' - ' . $post['post'];
+            }
+        }
+    } else {
+        echo "Valor do id {$id} inválido";
+    }
+});
